a900f5b1c04785be60152354a1c026d1c35cf3a0
mannschaft <38758010+mannschaft@users.noreply.github.com>
2018-04-26 15:54:05 +0200

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,40 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/Apps-firefox-icon.png') {
+      myImage.setAttribute ('src','images/Apps-firefox-icon-2.png');
+    } else {
+      myImage.setAttribute ('src','images/Apps-firefox-icon.png');
+    }
+}
+
+/*
+Everything in between is a comment.
+*/
+
+// This is a comment
+
+document.querySelector('h1').onclick = function() {
+    alert('Ouch! Stop poking me!');
+}
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Mozilla is cool, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
